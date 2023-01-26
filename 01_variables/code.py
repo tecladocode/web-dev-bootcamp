db1d2268a4b57f293feb5862095a4b0cc29e9056
michellecaner <michellecaner@gmail.com>
2023-01-26 10:47:44 -0600

    variables refresher

---
+++ 01_variables/code.py
@@ -0,0 +1,21 @@
+"""Python refresher course - Variables Section"""
+
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+name = "Rolf"
+
+name = "Bob"
+
+print(name)
+
+a = 25
+b = a
+
+print(b)
